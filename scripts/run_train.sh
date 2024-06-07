00067dda69d5d2a6edcedbcc3f2051c7dc4afb1a
Yiyang Zhou <74579698+YiyangZhou@users.noreply.github.com>
2024-06-08 00:26:19 +0800

    Update run_train.sh

--- scripts/run_train.sh
+++ scripts/run_train.sh
@@ -1,23 +1,13 @@
-#!/bin/bash -l
-#SBATCH --job-name=step_by_step_dpo_lora
-#SBATCH --time=5:00:00
-#SBATCH --ntasks-per-node=1
-#SBATCH --nodes=1
-#SBATCH --gpus-per-node=1
-#SBATCH --partition=compute
-#SBATCH --output=/data/yiyang_zhou/workplace/log
-#SBATCH --error=/data/yiyang_zhou/workplace/error_log
-
 # conda env 
-source activate /data/yiyang_zhou/miniconda3/envs/povid
-cd /data/yiyang_zhou/workplace/LLaVA/
+source activate *your conda path*/envs/csr
+
 deepspeed ./llava/train/train_dpo_lora.py \
     --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
     --deepspeed ./scripts/zero2.json \
-    --model_name_or_path /data/yiyang_zhou/workplace/LLaVA/checkpoint/llava-v1.5-7b \
+    --model_name_or_path liuhaotian/llava-v1.5-7b \
     --version v1 \
-    --data_path /data/yiyang_zhou/workplace/diffusion_safe/vlm_ft/dataset/safety_vlm_finetune_data/llava_7b_judge_claude.json \
-    --image_folder /data/yiyang_zhou/workplace/diffusion_safe/vlm_ft/dataset/safety_vlm_finetune_data/images \
+    --data_path ./data/*your data* \
+    --image_folder ./data/images/train2014 \
     --vision_tower openai/clip-vit-large-patch14-336 \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
@@ -26,8 +16,8 @@ deepspeed ./llava/train/train_dpo_lora.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir /data/yiyang_zhou/workplace/diffusion_safe/vlm_ft/dataset/safety_vlm_finetune_data/ckpt/llava_7b_lora_claude \
-    --num_train_epochs 3 \
+    --output_dir ./ckpt/*your ckpt name* \
+    --num_train_epochs 1 \
     --per_device_train_batch_size 1\
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 1 \
